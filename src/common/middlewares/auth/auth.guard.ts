eab582f6f15e85c6b16ddd7bcccceaf519ef442b
Deadmanch <stepanova.nata.r@gmail.com>
2024-03-22 15:32:17 +0300

    Сделал Middleware(AuthGuard, RoleGuard, AuthMiddleware)

---
+++ src/common/middlewares/auth/auth.guard.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from 'express';
+import { UserMsgEnum } from '../../../enums/user.msg.enums';
+import { HTTPStatusCode } from '../../http.status-code.enum';
+import { IMiddleware } from '../middlewares.interface';
+
+export class AuthGuard implements IMiddleware {
+	execute(req: Request, res: Response, next: NextFunction): void {
+		if (req.user) {
+			return next();
+		}
+		res.status(HTTPStatusCode.BAD_REQUEST).send({ error: UserMsgEnum.USER_IS_NOT_AUTHORIZED });
+	}
+}
