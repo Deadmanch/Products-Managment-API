1c22dfd6850bc715ee5b9a24d06484ee3a486c40
Deadmanch <stepanova.nata.r@gmail.com>
2024-03-22 15:33:55 +0300

    Сделал ExceptionFilter

---
+++ src/errors/exception.filter.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { NextFunction } from 'express-serve-static-core';
+import { inject, injectable } from 'inversify';
+import 'reflect-metadata';
+import { TYPES } from '../common/dependency-injection/types';
+import { HTTPStatusCode } from '../common/http.status-code.enum';
+import { ILogger } from '../logger/logger.interface';
+import { IExceptionFilter } from './exception.filter.interface';
+import { HTTPError } from './http-errors';
+@injectable()
+export class ExceptionFilter implements IExceptionFilter {
+	constructor(@inject(TYPES.ILogger) private logger: ILogger) {}
+
+	catch(err: Error, req: Request, res: Response, next: NextFunction): void {
+		if (err instanceof HTTPError) {
+			this.logger.error(`[${err.context}] Ошибка ${err.statusCode} ${err.message}`);
+		} else {
+			this.logger.error(`${err.message}`);
+			res.status(HTTPStatusCode.SERVER_EXCEPTION).send({ err: err.message });
+		}
+	}
+}
