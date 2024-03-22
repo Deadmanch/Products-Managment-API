e7fe9343aeec43b794c3a02a80dd85d1f713675e
Deadmanch <stepanova.nata.r@gmail.com>
2024-03-22 15:33:17 +0300

    Сделал Enums ответа пользователю

---
+++ src/enums/user.msg.enums.ts
@@ -0,0 +1,7 @@
+export enum UserMsgEnum {
+	USER_IS_EXISTS = 'Пользователь с таким email уже существует',
+	USER_IS_NOT_EXIST = 'Пользователь с такими параметрами не существует. Проверьте введенные данные!',
+	USER_EMAIL_ERR = 'Неверно указан email',
+	USER_IS_NOT_ENOUGH_RIGHTS = 'Недостаточно прав для выполнения операции',
+	USER_IS_NOT_AUTHORIZED = 'Вы не авторизованы',
+}
