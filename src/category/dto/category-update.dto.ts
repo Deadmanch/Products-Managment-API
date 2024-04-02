7d362178271c240e5065ed7de55db8bd68ebfacc
Deadmanch <stepanova.nata.r@gmail.com>
2024-04-02 13:04:10 +0300

    Сделал Category логику

---
+++ src/category/dto/category-update.dto.ts
@@ -0,0 +1,20 @@
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import {
+	CATEGORY_IS_NOT_EXIST,
+	CATEGORY_NAME_ERR,
+	CATEGORY_NAME_NOT_SPECIFIED,
+	ID_CATEGORY_ERR,
+} from '../../constants/category.msg';
+
+export class CategoryUpdateDto {
+	@IsNumber(
+		{ allowNaN: false, allowInfinity: false, maxDecimalPlaces: 0 },
+		{ message: ID_CATEGORY_ERR },
+	)
+	@IsNotEmpty({ message: CATEGORY_IS_NOT_EXIST })
+	id: number;
+
+	@IsString({ message: CATEGORY_NAME_ERR })
+	@IsNotEmpty({ message: CATEGORY_NAME_NOT_SPECIFIED })
+	name: string;
+}
