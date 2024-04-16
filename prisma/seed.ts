e22a404a9baf0d44a567e25956e3e5a24f63d614
Deadmanch <stepanova.nata.r@gmail.com>
2024-04-16 11:48:20 +0300

    Добавлены map в модели + обновил seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -37,7 +37,7 @@ export default class Seed {
 		const productsSalty = [
 			{
 				title: 'Вегетарианский',
-				description: 'круассан, соус песто, руккола, Помидор, Сыр моцарелла, вяленые томаты',
+				description: 'круассан, соус песто, руккола, помидор, сыр моцарелла, вяленые томаты',
 				quantity: 10,
 				price: 350,
 				categoryId: 1,
@@ -46,7 +46,7 @@ export default class Seed {
 				title: 'Чизбургер',
 				description:
 					'круассан, говяжья котлета, сыр тостовый, салат айсберг, помидор, квашеный огурец, лук сушеный, соус острый / соус бургерный',
-				quantity: 13,
+				quantity: 10,
 				price: 400,
 				categoryId: 1,
 			},
@@ -54,8 +54,51 @@ export default class Seed {
 				title: 'Дабл Чизбургер',
 				description:
 					'круассан, говяжья котлета, сыр тостовый, салат айсберг, помидор, квашеный огурец, лук сушеный, соус острый / соус бургерный',
-				quantity: 7,
+				quantity: 10,
 				price: 500,
+				categoryId: 1,
+			},
+			{
+				title: 'Курица Терияки',
+				description: 'круассан, курица, салат, огурец, имбирь маринованный, кунжут, соус терияки',
+				quantity: 10,
+				price: 420,
+				categoryId: 1,
+			},
+			{
+				title: 'Амстердам',
+				description: 'круассан, сельдь, сыр моцарелла, помидор, салат, соус песто, соус чесночный',
+				quantity: 10,
+				price: 400,
+				categoryId: 1,
+			},
+			{
+				title: 'Королевский',
+				description: 'круассан, помидор, салат, сыр королевский, курица, соус горчичный',
+				quantity: 10,
+				price: 650,
+				categoryId: 1,
+			},
+			{
+				title: 'Филадельфия с лососем',
+				description: 'круассан, сыр Филадельфия, лосось, салат, огурец',
+				quantity: 10,
+				price: 700,
+				categoryId: 1,
+			},
+			{
+				title: 'Филадельфия с ветчиной',
+				description:
+					'круассан, сыр Филадельфия, ветчина, помидор, салат, яичница, соус классический',
+				quantity: 10,
+				price: 550,
+				categoryId: 1,
+			},
+			{
+				title: 'Цезарь с курицей',
+				description: 'круассан, курица, помидоры, сыр пармезан, салат айсберг, соус Цезарь',
+				quantity: 10,
+				price: 600,
 				categoryId: 1,
 			},
 		];
@@ -83,6 +126,13 @@ export default class Seed {
 				price: 600,
 				categoryId: 1,
 			},
+			{
+				title: 'Малиновое наслаждение',
+				description: 'круассан, сливочный крем, джем малина, мята, сахарная пудра',
+				quantity: 7,
+				price: 600,
+				categoryId: 1,
+			},
 		];
 		const drinks = [
 			{
@@ -102,6 +152,30 @@ export default class Seed {
 			{
 				title: 'Чай витаминный "апельсиновый с мятой"',
 				quantity: 7,
+				price: 120,
+				categoryId: 1,
+			},
+			{
+				title: 'Коктейль молочный с бананом"',
+				quantity: 10,
+				price: 130,
+				categoryId: 1,
+			},
+			{
+				title: 'Коктейль молочный с бананом и ягодами"',
+				quantity: 2,
+				price: 120,
+				categoryId: 1,
+			},
+			{
+				title: 'Лимонад классический"',
+				quantity: 2,
+				price: 130,
+				categoryId: 1,
+			},
+			{
+				title: 'Чай "Сэр Чарльз Грей"',
+				quantity: 2,
 				price: 120,
 				categoryId: 1,
 			},
