58efe73c48a342a29665440f3276abd237d45464
Deadmanch <stepanova.nata.r@gmail.com>
2024-04-16 11:52:32 +0300

    Сделал сцену корзины в тг боте

--- src/telegram-bot/scenes/cart/cart.dictionary.ts
+++ src/telegram-bot/scenes/cart/cart.dictionary.ts
@@ -6,7 +6,7 @@ export const CART_EMPTY_MSG = 'Ваша корзина пуста';
 export const ADD_QUANTITY_ACTION = 'add_quantity_';
 export const ADD_QUANTITY_TEXT = '+';
 export const REMOVE_QUANTITY_ACTION = 'remove_quantity_';
-export const REMOVE_QUANTITY_TEXT = '+';
+export const REMOVE_QUANTITY_TEXT = '-';
 
 export const REMOVE_FROM_CART_ACTION = 'remove_from_cart_';
 export const REMOVE_FROM_CART_TEXT = 'Удалить из корзины';
@@ -15,3 +15,11 @@ export const REMOVE_FROM_CART_SUCCESS = 'Товар успешно удален 
 export const PRODUCT_NOT_FOUND_ERR = 'Товар отсутствует в корзине';
 
 export const CART_NAME_MSG = 'Показать текущую корзину';
+
+export const CLEAR_CART_ACTION = 'clear_cart';
+export const CLEAR_CART_TEXT = 'Очистить корзину';
+export const CLEAR_CART_SUCCESS = 'Корзина очищена';
+
+export const CHECKOUT_ACTION = 'checkout';
+export const CHECKOUT_TEXT = 'Оформить заказ';
+export const CHECKOUT_SUCCESS = 'Поздравляем вас\\! Заказ оформлен';
--- src/telegram-bot/scenes/cart/cart.scene.ts
+++ src/telegram-bot/scenes/cart/cart.scene.ts
@@ -5,21 +5,28 @@ import { TYPES } from '../../../common/dependency-injection/types';
 import { ILogger } from '../../../logger/logger.interface';
 import { IProductRepository } from '../../../products/interface/products.repository.interface';
 import { Product } from '../../../products/product.entity';
-import { EmptyDeliveryAddressError } from '../../errors/deliverty-error/empty-deliveryAdress.error';
 import { IBotContext } from '../../interface/bot-context.interface';
 import { IScene } from '../../interface/scene.interface';
 import { CartItemType } from '../../types/cart-item.type';
-import { DeliveryView } from '../delivery/delivery.view';
-import { SHOW_PRODUCT_DETAIL, SHOW_PRODUCT_DETAIL_ACTION } from '../menu/menu-list.dictionary';
+import { DELIVERY_NAME } from '../delivery/delivery.dictionary';
+import { EmptyDeliveryAddressError } from '../delivery/empty-deliveryAdress.error';
+import { MENU_ITEM_ACTIONS_LIST } from '../menu/menu-list.dictionary';
 import { ProductView } from '../menu/menu-product-view';
+import { BACK_TO_START_ACTION, BACK_TO_START_MSG, START_NAME } from '../start/start.scene.enum';
 import {
 	ADD_QUANTITY_ACTION,
 	ADD_QUANTITY_TEXT,
 	CART_EMPTY_MSG,
 	CART_NAME,
 	CART_START_MSG,
+	CHECKOUT_ACTION,
+	CHECKOUT_SUCCESS,
+	CHECKOUT_TEXT,
+	CLEAR_CART_ACTION,
+	CLEAR_CART_TEXT,
 	PRODUCT_NOT_FOUND_ERR,
 	REMOVE_FROM_CART_ACTION,
+	REMOVE_FROM_CART_SUCCESS,
 	REMOVE_FROM_CART_TEXT,
 	REMOVE_QUANTITY_ACTION,
 	REMOVE_QUANTITY_TEXT,
@@ -42,11 +49,11 @@ export class CartScene implements IScene {
 					throw new EmptyDeliveryAddressError();
 				}
 				await ctx.replyWithMarkdownV2(CART_START_MSG);
-				await ctx.replyWithMarkdownV2(DeliveryView.getDeliveryAddress(ctx.session.deliveryAddress));
-
-				await (ctx.session.cart.items
-					? this.showCartProducts(ctx.session.cart.items, ctx)
-					: this.showEmptyCart(ctx));
+				if (ctx.session.cart.items.length > 0) {
+					await this.showCartProducts(ctx.session.cart.items, ctx);
+				} else {
+					await this.showEmptyCart(ctx);
+				}
 			});
 
 			cartScene.action(/add_quantity_(\d+)/, async (ctx) => {
@@ -63,10 +70,12 @@ export class CartScene implements IScene {
 					}
 					return isFound;
 				});
+
 				if (!cartItem) {
 					throw new Error(PRODUCT_NOT_FOUND_ERR);
 				}
-				if (product && product?.quantity && product.quantity >= cartItem.quantity) {
+
+				if (product && product?.quantity && product.quantity > cartItem.quantity) {
 					await this.editCartItemView(ctx, productId, product, cartItem);
 				}
 			});
@@ -79,7 +88,7 @@ export class CartScene implements IScene {
 				const product = await this.productRepository.getById(productId);
 				const cartItem = ctx.session.cart.items.find((cartItem, index, items) => {
 					const isFound = cartItem.productId === productId;
-					if (isFound && cartItem.quantity > 0) {
+					if (isFound && cartItem.quantity > 1) {
 						cartItem.quantity -= 1;
 						items[index] = cartItem;
 					}
@@ -92,8 +101,58 @@ export class CartScene implements IScene {
 					await this.editCartItemView(ctx, productId, product, cartItem);
 				}
 			});
+			cartScene.action(CLEAR_CART_ACTION, async (ctx) => {
+				ctx.session.cart.items = [];
+				await ctx.replyWithMarkdownV2(CART_EMPTY_MSG);
+				await ctx.scene.enter(START_NAME);
+			});
+			cartScene.action(CHECKOUT_ACTION, async (ctx) => {
+				for (const cartItem of ctx.session.cart.items) {
+					const product = await this.productRepository.getById(cartItem.productId);
+					if (product) {
+						await this.productRepository.editQuantity(
+							product.id,
+							product.quantity - cartItem.quantity,
+						);
+					}
+				}
+				ctx.session.cart.items = [];
+				await ctx.replyWithMarkdownV2(CHECKOUT_SUCCESS);
+				await ctx.scene.enter(START_NAME);
+			});
+			cartScene.action(/remove_from_cart_(\d+)/, async (ctx) => {
+				const productId = Number(ctx.match[1]);
+				if (!ctx.session.cart.items) {
+					throw new Error(CART_EMPTY_MSG);
+				}
+				const index = ctx.session.cart.items.findIndex(
+					(cartItem) => cartItem.productId === productId,
+				);
+				if (index >= 0) {
+					ctx.session.cart.items.splice(index, 1);
+					const items = ctx.session.cart.items;
+					await ctx.replyWithMarkdownV2(REMOVE_FROM_CART_SUCCESS);
+					if (!items.length) {
+						await ctx.replyWithMarkdownV2(CART_EMPTY_MSG);
+						await ctx.scene.enter(START_NAME);
+					} else {
+						await this.showCartProducts(items, ctx);
+					}
+				} else {
+					throw new Error(PRODUCT_NOT_FOUND_ERR);
+				}
+			});
+			cartScene.action(BACK_TO_START_ACTION, async (ctx) => {
+				ctx.session.cart.items = [];
+				await ctx.scene.enter(START_NAME);
+			});
 		} catch (e) {
 			cartScene.leave();
+			if (e instanceof EmptyDeliveryAddressError) {
+				cartScene.enter(async (ctx) => {
+					await ctx.scene.enter(DELIVERY_NAME);
+				});
+			}
 			if (e instanceof Error) {
 				this.loggerService.error(e.message);
 				cartScene.enter(async (ctx) => {
@@ -127,18 +186,30 @@ export class CartScene implements IScene {
 			);
 
 			const productItemsButtons = [
-				Markup.button.callback(ADD_QUANTITY_TEXT, ADD_QUANTITY_ACTION + product.id),
-				Markup.button.callback(REMOVE_QUANTITY_TEXT, REMOVE_QUANTITY_ACTION + productId),
-				Markup.button.callback(REMOVE_FROM_CART_TEXT, REMOVE_FROM_CART_ACTION + productId),
-				Markup.button.callback(SHOW_PRODUCT_DETAIL, SHOW_PRODUCT_DETAIL_ACTION + productId),
+				[
+					Markup.button.callback(REMOVE_QUANTITY_TEXT, REMOVE_QUANTITY_ACTION + productId),
+					Markup.button.callback(ADD_QUANTITY_TEXT, ADD_QUANTITY_ACTION + product.id),
+				],
+				[Markup.button.callback(REMOVE_FROM_CART_TEXT, REMOVE_FROM_CART_ACTION + productId)],
 			];
 			await ctx.replyWithMarkdownV2(productListItem, Markup.inlineKeyboard(productItemsButtons));
 			await new Promise((resolve) => setTimeout(resolve, 500));
 		}
+		await this.showCartActions(ctx);
+	}
+
+	private async showCartActions(ctx: IBotContext): Promise<void> {
+		const keyBoard = [
+			[Markup.button.callback(CHECKOUT_TEXT, CHECKOUT_ACTION)],
+			[Markup.button.callback(CLEAR_CART_TEXT, CLEAR_CART_ACTION)],
+			[Markup.button.callback(BACK_TO_START_MSG, BACK_TO_START_ACTION)],
+		];
+		await ctx.replyWithMarkdownV2(MENU_ITEM_ACTIONS_LIST, Markup.inlineKeyboard(keyBoard));
 	}
 
 	private async showEmptyCart(ctx: IBotContext): Promise<void> {
-		await ctx.reply(CART_EMPTY_MSG);
+		await ctx.replyWithMarkdownV2(CART_EMPTY_MSG);
+		await ctx.scene.enter(START_NAME);
 	}
 
 	private async defineCart(ctx: IBotContext): Promise<void> {
@@ -158,10 +229,11 @@ export class CartScene implements IScene {
 		cartItem: CartItemType,
 	): Promise<void> {
 		const keyBoard = [
-			Markup.button.callback(ADD_QUANTITY_TEXT, ADD_QUANTITY_ACTION + productId),
-			Markup.button.callback(REMOVE_QUANTITY_TEXT, REMOVE_QUANTITY_ACTION + productId),
-			Markup.button.callback(REMOVE_FROM_CART_TEXT, REMOVE_FROM_CART_ACTION + productId),
-			Markup.button.callback(SHOW_PRODUCT_DETAIL, SHOW_PRODUCT_DETAIL_ACTION + productId),
+			[
+				Markup.button.callback(REMOVE_QUANTITY_TEXT, REMOVE_QUANTITY_ACTION + productId),
+				Markup.button.callback(ADD_QUANTITY_TEXT, ADD_QUANTITY_ACTION + productId),
+			],
+			[Markup.button.callback(REMOVE_FROM_CART_TEXT, REMOVE_FROM_CART_ACTION + productId)],
 		];
 		await ctx.editMessageText(
 			ProductView.getCartListView(
